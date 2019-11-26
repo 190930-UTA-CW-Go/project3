e18de54de3e94b6affb80278b402d635c8ef3f40
Garner Deng <56040329+ggarnerdeng@users.noreply.github.com>
2019-11-26 15:40:36 -0600

    Add files via upload
    Users can now edit their own portfolios by typing in the portfolio file name into the HTML website form. 
    HTML navigation has been implemented. 
    ALL created or edited portfolios are now automatically uploaded to the Amazon server. More details in README

--- API/main.go
+++ API/main.go
@@ -54,8 +54,18 @@ var education = Education{}
 var project = Project{}
 var jsonfile string //Sets the value of the jsonfile variable.
 
+const remoteHostname = "18.188.174.65" //IP address of amazon server
+const amazon = "ec2-user@ec2-18-188-174-65.us-east-2.compute.amazonaws.com"
+
+//This is the amazon aws hostname@IP address.
+const key = "rego.pem" //This is security key used to login/SSH
+
+/////////////////////////////////////IMPORTANT/////////////////////////////////////////////////
+const localuser = "AwYiss" //USER'S LOGIN NAME. HARDCODED FOR NOW
+/////////////////////////////////////IMPORTANT/////////////////////////////////////////////////
+
 func main() {
-	//http.Handle("/", http.FileServer(http.Dir(".")))
+	//http.Handle("/", http.FileServer(http.Dir("."))) Nathan's code. Dont know if its important
 	http.HandleFunc("/portfolioform", portfolioform)
 	http.HandleFunc("/", index)
 
@@ -73,7 +83,7 @@ func index(response http.ResponseWriter, request *http.Request) {
 	temp.Execute(response, nil)
 }
 
-//The portfolioform function displays the portfolio form at localhost:8080/portfolioform
+//portfolioform function displays the portfolio form at localhost:8080/portfolioform
 func portfolioform(response http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("templates/portfolioform.html")
 	if err != nil {
@@ -82,20 +92,22 @@ func portfolioform(response http.ResponseWriter, request *http.Request) {
 	temp.Execute(response, nil)
 }
 
-//The formsubmitted function lets the user know their portfolio was submitted successfully.
+/*formsubmitted function lets the user know their portfolio was submitted successfully.
+Also automatically uploads/overwrites the user portfolio onto the amazon server*/
 func formsubmitted(response http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("templates/formsubmitted.html")
 	if err != nil {
 		panic(err)
 	}
 
-	//Take inputted fullname from profile and use it to name the json file.
-	info.Name = request.FormValue("fullname") //Part of information structure of profile.
+	//Take inputted fullname from portfolio and use it to name the json file.
+	info.Name = request.FormValue("fullname") //Part of information structure
 	filename := info.Name
 	filename = strings.Replace(filename, " ", "_", -1) //Changes white space to underscores
-	filename = filename + ".json"
+	filename = filename + ".json"                      //new filename always has underscores instead of wyt space
 
-	//removes file if it previously existed, to overwrite instead of append
+	//Removes file if it previously existed, to "overwrite" instead of append.
+	//If the file existed before, without this code, information would be appended to old file.
 	remote3 := exec.Command("rm", filename)
 	remote3.Run()
 
@@ -107,25 +119,28 @@ func formsubmitted(response http.ResponseWriter, request *http.Request) {
 	}
 	defer f.Close()
 
+	//Pull information from HTML form, set to global variables.
 	info.Title = request.FormValue("jobtitle")
 	info.Email = request.FormValue("email")
 	info.Phone = request.FormValue("phone")
-
 	about.Aboutme = request.FormValue("aboutme")
-
 	education.College = request.FormValue("college")
 	education.Degree = request.FormValue("degree")
-
 	project.Name = request.FormValue("projectname")
 	project.Tech = request.FormValue("techused")
 	project.Desc = request.FormValue("projectdesc")
-
 	portfolio.Information = info
 	portfolio.About = about
 	portfolio.Education = education
 	portfolio.Project = project
-	portfolio.Status = "UNCHECKED"
 
+	portfolio.Status = "UNCHECKED" /*	This is not seen by the user in the HTML interface.
+		While the user can manually edit the json file using
+		Notepad, there is no other way for the user to interact
+		with this field variable. Admins can change this variable
+		to "ACCEPTED" or "DENIED" using the Approve-Deny-Portfolio
+		program. */
+	//Json file formatting
 	b, err := json.MarshalIndent(portfolio, "", "    ")
 	if err != nil {
 		http.Error(response, err.Error(), 500)
@@ -142,41 +157,53 @@ func formsubmitted(response http.ResponseWriter, request *http.Request) {
 	fmt.Println(portfolio.Project)
 	fmt.Println(portfolio)
 	temp.Execute(response, portfolio)
+
+	//make user portfolio on amazon. If it already exists, nothing happens
+	remote4 := exec.Command("ssh", "-i", key, amazon, "mkdir", "-p", "Portfolios/"+localuser)
+	remote4.Run()
+	//move file to amazon server. If it already exist, gets overwritten
+	remote2 := exec.Command("scp", "-i", key, filename, amazon+":Portfolios/"+localuser)
+	remote2.Run()
 }
 
-//The editportfolio function takes info from an existing .json file and displays it in the form at localhost:8080/editportfolio
+/*editportfolio function takes info from an existing .json file and displays it in the form
+at localhost:8080/editportfolio. This edits the LOCAL copy of the portfolio. Then upon
+hitting the submit button again, BOTH local copy here and the remote copy on amazon
+are overwritten with the new information.*/
 func editportfolio(response http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("templates/editportfolio.html")
 	if err != nil {
 		panic(err)
 	}
-	jsonfile = request.FormValue("filename") //Part of information structure of profile.
+	jsonfile = request.FormValue("filename") //request filename to be modifed by the user
 
 	//Opens the .json file.
 	readjson, readerr := os.Open(jsonfile)
 	if readerr != nil {
 		panic(readerr)
 	}
-
 	defer readjson.Close()
+
 	//Reads the .json file.
 	jsonvalue, _ := ioutil.ReadAll(readjson)
+
 	//Grabs the info out of the .json file.
 	json.Unmarshal(jsonvalue, &portfolio)
+
 	//Prints the values of variables in the console to make sure the .json file was read properly.
 	fmt.Println(portfolio)
 	fmt.Println(portfolio.About.Aboutme)
-
 	temp.Execute(response, portfolio)
 }
 
-//The chooseportfolio function prompts user to type in portfolio filename to edit.
+//chooseportfolio function prompts user to type in portfolio filename to edit.
 func chooseportfolio(response http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("templates/chooseportfolio.html")
 	if err != nil {
 		panic(err)
 	}
-	//Take inputted fullname from profile and use it to name the json file.
+
+	//Requesting filename information from HTML form
 	jsonfile = request.FormValue("filename") //Part of information structure of profile.
 	temp.Execute(response, portfolio)
 }
